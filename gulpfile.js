f884b49acc39cdbacbaaf7bbbe94aa74db42a6bf
Jacob Jenkins <jacob@jacobejenkins.com>
2016-02-19 22:38:48 -0600

    Add gulpfile and package.json

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var ts = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
+
+var tsProject = ts.createProject({
+    module: "commonjs",
+    target: "es5",
+    preserveConstEnums: true,
+    sourceMap: true
+});
+
+gulp.task('typescript server', ['prettify'], function() {
+    var result = gulp.src('src/**/*.ts')
+            .pipe(sourcemaps.init())
+            .pipe(ts(tsProject));
+
+    return result.js
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/'));
+});
+
+// Start up the application
+gulp.task('start' ,['build'], function() {
+    nodemon({
+        script: 'app/app.js',
+        watch: [
+            'app/**'
+        ]
+    });
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/app/**/*.ts', ['typescript server']);
+});
+
+gulp.task('build', ['typescript']);
+gulp.task('default', ['build', 'start', 'watch']);
