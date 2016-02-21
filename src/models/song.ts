aea63646ab43943ae473a8aefaddaebf427d0b72
Jacob Jenkins <jacob@jacobejenkins.com>
2016-02-20 20:42:18 -0600

    Fix db problems

--- src/models/song.ts
+++ src/models/song.ts
@@ -10,8 +10,8 @@ var SongSchema = new Schema({
     length: Number, // Length in Seconds
     numberOfChunks: Number, // Total number of chunks to split file into
     fixedChunkSize: Number, // Size in bytes
-    fileType: Number, // File extension, eg 'mp3'
-    fileSize: String,
+    fileType: String, // File extension, eg 'mp3'
+    fileSize: Number,
     path: String
 });
 
