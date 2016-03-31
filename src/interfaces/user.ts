f1fac5905823699c3b6095f915d7cb66e5bbe6fc
James Risberg <jimmyrisberg56@gmail.com>
2016-03-30 22:50:06 -0400

    Update user model

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -3,8 +3,53 @@
 import mongoose = require('mongoose');
 
 interface IUser extends mongoose.Document {
-    username: string;
-    password: string;
+    firstName: {
+        'type': String,
+        'trim': true,
+        'default': '',
+        'validate': [validateLocalStrategyProperty, 'Please fill in your first name']
+    },
+    lastName: {
+        'type': String,
+        'trim': true,
+        'default': '',
+        'validate': [validateLocalStrategyProperty, 'Please fill in your last name']
+    },
+    email: {
+        'type': String,
+        'trim': true,
+        'unique': true,
+        'lowercase': true,
+        'default': '',
+        'validate': [validateLocalStrategyEmail, 'Not a valid email address']
+    },
+    course: {
+        'type': String,
+        'trim': true,
+        'default': ''
+    },
+    roles: {
+        'type': [{
+            'type': String,
+            'enum': ['student', 'entranceProctor', 'examProctor', 'admin']
+        }],
+        'default': ['student']
+    },
+    password: {
+        'type': String,
+        'default': ''
+    },
+    salt: {
+        'type': String
+    },
+    provider: {
+        'type': String,
+        'required': 'Provider is required'
+    },
+    online: {
+        'type': Boolean,
+        'default': false
+    }
 }
 
 export = IUser;
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,111 @@
 /// <reference path="../../typings/main.d.ts"/>
 
 import mongoose = require('mongoose');
-import ISong = require('../interfaces/user');
+import validator = require('validator');
+import crypto = require('crypto');
+import IUser = require('../interfaces/user');
 var Schema = mongoose.Schema;
+var uniqueValidator = require('mongoose-unique-validator');
 
+/**
+ * A Validation function for local strategy properties
+ */
+function validateLocalStrategyProperty(property) {
+    return ((this.provider !== 'local' && !this.updated) || property.length);
+};
+
+/**
+ * A Validation function for local strategy email
+ */
+function validateLocalStrategyEmail(email) {
+    return ((this.provider !== 'local' && !this.updated) || validator.isEmail(email));
+};
+
+/**
+ * User schema for proctors and students.
+ */
 var UserSchema = new Schema({
-    username: String,
-    password: String
+    firstName: {
+        'type': String,
+        'trim': true,
+        'default': '',
+        'validate': [validateLocalStrategyProperty, 'Please fill in your first name']
+    },
+    lastName: {
+        'type': String,
+        'trim': true,
+        'default': '',
+        'validate': [validateLocalStrategyProperty, 'Please fill in your last name']
+    },
+    email: {
+        'type': String,
+        'trim': true,
+        'unique': true,
+        'lowercase': true,
+        'default': '',
+        'validate': [validateLocalStrategyEmail, 'Not a valid email address']
+    },
+    course: {
+        'type': String,
+        'trim': true,
+        'default': ''
+    },
+    roles: {
+        'type': [{
+            'type': String,
+            'enum': ['student', 'entranceProctor', 'examProctor', 'admin']
+        }],
+        'default': ['student']
+    },
+    password: {
+        'type': String,
+        'default': ''
+    },
+    salt: {
+        'type': String
+    },
+    provider: {
+        'type': String,
+        'required': 'Provider is required'
+    },
+    online: {
+        'type': Boolean,
+        'default': false
+    }
 });
+
+/**
+ * Hook a pre save method to hash the password
+ */
+UserSchema.pre('save', function(next) {
+    if (this.password && this.isModified('password')) {
+        this.salt = crypto.randomBytes(16).toString('base64');
+        this.password = this.hashPassword(this.password);
+    }
+
+    next();
+});
+
+/**
+ * Create instance method for hashing a password
+ * @param: {String} password
+ */
+UserSchema.method('hashPassword', function(password: string) {
+    if (this.salt && password) {
+        return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+    } else {
+        return password;
+    }
+});
+
+/**
+ * Create instance method for authenticating user
+ * @param: {String} password
+ */
+UserSchema.method('authenticate', function(password: string) {
+    return this.password === this.hashPassword(password);
+});
+
+UserSchema.plugin(uniqueValidator);
 
 export = mongoose.model<IUser>('User', UserSchema);
